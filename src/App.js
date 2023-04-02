7c118b7cfb6c7cfef6a3a0fbe259d6415c1ec9f7
samarijo <sabrinamariano1994@hotmail.com>
2023-04-02 10:07:54 -0300

    estilização da barra de nav ok

--- src/App.js
+++ src/App.js
@@ -4,19 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Sabrina
-        </a>
+      <header className="App-header"> 
+            <p className='header-text'>SOBRE</p>
+            <img className='header-icon-nav' src='icon-nav.png'></img>
       </header>
     </div>
   );
